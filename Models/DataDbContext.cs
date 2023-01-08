b52eb1e251a9c3c316f091f5f4bd8c68c4300d8f
qasimzaheer-124 <20101001-124@uskt.edu.pk>
2023-01-08 15:14:13 +0500

    Add project files.

---
+++ Models/DataDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace FabSet_Project.Models
+{
+    public class DataDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DataDbContext(DbContextOptions<DataDbContext> options) : base(options) //configure dbContext option
+        {
+
+        }
+        public Microsoft.EntityFrameworkCore.DbSet<Signup> Signup { get; set; } // add your data models
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FabSet_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
